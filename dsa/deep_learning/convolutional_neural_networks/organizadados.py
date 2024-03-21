b66834933b818a905203a7fbca56d8d5a47a9578
Hugo Braga <hugobraga@gmail.com>
2024-03-20 22:10:40 -0400

    Approached multiprocessing pickling problems with separate file

---
+++ dsa/deep_learning/convolutional_neural_networks/organizadados.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+# Defina um organizador de dados para modelo PyTorch 
+class OrganizaDados(Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+
+        X = Image.open(self.df['path'][index])
+        y = torch.tensor(int(self.df['cell_type_idx'][index]))
+
+        if self.transform:
+            X = self.transform(X)
+
+        return X, y
